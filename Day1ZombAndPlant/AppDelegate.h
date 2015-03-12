d860a2171590f48ac78ddf60dafdf22c5260b96c
tarena <tarena@a2.local>
2015-03-12 15:03:12 +0800

    Initial Commit

---
+++ Day1ZombAndPlant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Day1ZombAndPlant
+//
+//  Created by tarena on 15-3-12.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
